d2f68af4e6acb93c9df981cdf6c4d7495ae33bcf
hpt <hpt@1995bbac-7161-4734-a539-528d3d2fb522>
2007-07-03 03:49:51 +0000

    Add the support for open blade center's vterm.

--- recorder.tcl
+++ recorder.tcl
@@ -2,4 +2,5 @@ set apollolp1 {"telnet apollov1" "padmin" "padmin" "rmvt -id 3" "mkvt -id 3"}
 set clashlp2 {"telnet sqh20lte" "hscroot" "abc123" "rmvterm -m fsp-clash -p clashlp2" "mkvterm -m fsp-clash -p clashlp2"}
 set venuslp2 {"telnet sqh11lte" "hscroot" "abc123" "rmvterm -m fsp-venus -p venuslp2" "mkvterm -m fsp-venus -p venuslp2"}
 set dreamlp1 {"ssh hscroot@hmc-eclip" "" "abc123" "rmvterm -m fsp-dream -p dreamlp1" "mkvterm -m fsp-dream -p dreamlp1"}
+set uli08 {"telnet uli" "USERID" "PASSW0RD" "" "console -o -T blade[8]"}
 
