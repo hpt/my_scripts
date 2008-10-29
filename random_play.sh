e5f87e28eb951606989236e2d8606740cc735be2
hpt <hpt@1995bbac-7161-4734-a539-528d3d2fb522>
2008-10-29 06:08:48 +0000

    Using array to solve this problem.

--- random_play.sh
+++ random_play.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 n=$(ls -1 /home/hpt/Music/*.mp3|wc -l);
-n=$(echo "$RANDOM % $n + 1"|bc);
-i=0;
 IFS=$'\n'
-for f in $(ls -1 /home/hpt/Music/*.mp3);
-do 
-    ((i++)); 
-    [[ $i -lt $n ]] && continue;
-    echo $f;
-    mpg321 "$f";
-    break;
-done
+musics=($(ls -1 /home/hpt/Music/*.mp3));
+n=$(echo "$RANDOM % $n + 1"|bc);
+mpg321 "${musics[n-1]}";
